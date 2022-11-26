88ea8a38f56cbf219fff707fc1f17800c6d688d8
vadim <89281779909@mail.ru>
2022-11-26 18:58:53 +0300

    task5

--- seminar2/task4.py
+++ seminar2/task4.py
@@ -1,12 +1,32 @@
 # 4.	Напишите программу, которая проверяет пятизначное число на палиндром.
-count = 0
+
+
+# f = 1
+# n = input('Введите число ')
+# for i in range(len(n) // 2):
+#     if n[i] != n[-i - 1]:
+#         print('НЕ Палиндром')
+#         f = 0
+#         break
+# if f:
+#     print('Палиндром')
+
+
+# count = 0
+# n = input('Введите число ')
+# for i in range(len(n) // 2):
+#     if n[i] == n[-i - 1]:
+#         count += 1
+# if count == len(n) // 2:
+#     print('Палиндром')
+# else:
+#     print('НЕ Палиндром')
+
+
 n = input('Введите число ')
-for i in range(len(n) // 2):
-    if n[i] == n[-i - 1]:
-        count += 1
-if count == len(n) // 2:
+n2 = n[::-1]
+if n2 == n:
     print('Палиндром')
 else:
     print('НЕ Палиндром')
-
 
