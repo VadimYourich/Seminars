46d5d356a3b9407f4da7b505c2189ff3e4f3015c
vadim <89281779909@mail.ru>
2022-11-27 15:30:22 +0300

    task6

--- seminar2/task6.py
+++ seminar2/task6.py
@@ -1,7 +1,17 @@
 # 6.	Напишите программу, в которой пользователь будет задавать две строки, а программа -
 # определять количество вхождений одной строки в другой.
 
-n1 = 'vvrrjklffggdddtttedssswaaaajjjfffgghhbbbvvvccccccxxxddeeerttyyuuiiokkjjhhgg'
-n2 = 'c'
+# n1 = 'vvrrjklffggdddtttedssswaaaajjjfffgghhbbbvvvccccccxxxddeeerttyyuuiiokkjjhhgg'
+# n2 = 'c'
+# print(n1.count(n2))
 
-print(n1.count(n2))
+n1 = 'vvrrjklffggdddtttedssswaaaajjjfffgghhbbbvvvccccccxxxddeeerttyyuuiiokkjjhhgg'
+n2 = 'vv'
+count = 0
+i = 0
+while i < len(n1):
+    if n1[i:i + len(n2)] == n2:
+        count += 1
+        i += (len(n2) - 1)
+    i += 1
+print(count)
