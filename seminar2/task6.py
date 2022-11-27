039cfe220202bc582e18b16c3041a89be5a680ae
vadim <89281779909@mail.ru>
2022-11-27 14:27:52 +0300

    task6

---
+++ seminar2/task6.py
@@ -0,0 +1,7 @@
+# 6.	Напишите программу, в которой пользователь будет задавать две строки, а программа -
+# определять количество вхождений одной строки в другой.
+
+n1 = 'vvrrjklffggdddtttedssswaaaajjjfffgghhbbbvvvccccccxxxddeeerttyyuuiiokkjjhhgg'
+n2 = 'c'
+
+print(n1.count(n2))
