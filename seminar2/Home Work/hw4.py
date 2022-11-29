66d8dfb6b04212fcd044fb77292beb275fe80be8
vadim <89281779909@mail.ru>
2022-11-29 23:15:03 +0300

    произведение элементов на указанных в файле позициях

--- seminar2/Home Work/hw4.py
+++ seminar2/Home Work/hw4.py
@@ -1,3 +1,31 @@
 # Задайте список из N элементов, заполненных числами из промежутка [-N, N].
 # Найдите произведение элементов на указанных позициях.
 # Позиции хранятся в файле file.txt в одной строке одно число.
+
+n = int(input('Введите число N: '))
+sp = []
+for i in range(-n, n + 1):
+    sp.append(i)
+print(sp)
+
+print('Введите', int(n / 2), 'позиции элементов в диапазоне от ', 0, ' до ', n * 2 + 1, ': ')
+sp2 = []
+for i in range(int(n / 2)):
+    sp2.append(input())
+# print(sp2)
+
+file = open('file.txt','w')
+def value(items):
+    for item in items:
+        print(item, file=file)
+    file.close()
+
+value(sp2)
+
+pr = 1
+with open('file.txt') as f:
+    for line in f:
+        pr *= sp[int(line)]
+print('Произведение элементов на указанных позициях равно: ', pr)
+
+
