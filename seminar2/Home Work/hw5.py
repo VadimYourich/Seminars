1f68ce3ab227789d81f73895ca460717efe87444
vadim <89281779909@mail.ru>
2022-11-30 18:34:03 +0300

    алгоритм перемешивания списка

--- seminar2/Home Work/hw5.py
+++ seminar2/Home Work/hw5.py
@@ -1,3 +1,16 @@
 # Реализуйте алгоритм перемешивания списка(shuffle использовать нельзя, другие методы из библиотеки random - можно).
 
 
+n = int(input('Введите колличество элементов нового списка: '))
+print('Введите', n, 'элементов: ')
+sp = []
+for i in range(n):
+    sp.append(input())
+print('Оригинальный список', sp)
+
+import random
+for i in range(n):
+    t = random.randint(0, n - 1)
+    sp[i], sp[t] = sp[t], sp[i]
+print('Перемешанный список', sp)
+
