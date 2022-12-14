f09da0aada9370332c540c22324f3ccfe559388d
vadim <89281779909@mail.ru>
2022-12-14 21:01:35 +0300

    слово, которое в тексте # встречается чаще всего

---
+++ seminar4/task6.py
@@ -0,0 +1,16 @@
+# 6.	Дан текст: в первой строке задано число строк, далее идут сами строки. Выведите слово, которое в этом тексте
+# встречается чаще всего. Если таких слов несколько, выведите то, которое меньше в лексикографическом порядке.
+# Входные данные	                        Выходные данные
+# 1
+# apple orange banana banana orange	        banana
+my_list = ['apple', 'orange', 'banana', 'banana', 'orange', 'banana']
+slov = {}
+for el in my_list:
+    slov[el] = slov.get(el, 0) + 1
+print(slov)
+slov = dict(sorted(slov.items()))
+print(slov)
+maximum = max(slov.values())
+for key, value in slov.items():
+    if value == maximum:
+        print(key)
