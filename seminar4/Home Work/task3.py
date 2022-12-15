a846a017c8781a80c0d6198ecf9f94f4f9c8caaa
vadim <89281779909@mail.ru>
2022-12-15 19:29:16 +0300

    выведет список неповторяющихся элементов исходной последовательности

--- seminar4/Home Work/task3.py
+++ seminar4/Home Work/task3.py
@@ -1,2 +1,15 @@
 # Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся
 # элементов исходной последовательности.
+
+print('Вводите числа, по окончанию введите 0: ')
+n = None
+list = []
+while n != 0:
+    n = int(input())
+    list.append(n)
+print(list)
+new_list = []
+for i in list:
+     if list.count(i) == 1:
+         new_list.append(i)
+print(new_list)
