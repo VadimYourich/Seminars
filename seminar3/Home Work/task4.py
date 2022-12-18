18809e9c97579f9d5aebf4c77546721d7bb71be2
vadim <89281779909@mail.ru>
2022-12-18 21:55:24 +0300

    преобразовать десятичное число в двоичное

--- seminar3/Home Work/task4.py
+++ seminar3/Home Work/task4.py
@@ -6,11 +6,25 @@
 # o	3 -> 11
 # o	2 -> 10
 
+# n = int(input('Введите целое число: '))
+# osn = 2
+# list = []
+# while n != 0:
+#     list.append(n % osn)
+#     n //= osn
+# list.reverse()
+# print(*list, sep='')
+
 n = int(input('Введите целое число: '))
 osn = 2
-list = []
-while n != 0:
-    list.append(n % osn)
+res = ''
+while n > 0:
+    res = str(n % osn) + res
     n //= osn
-list.reverse()
-print(*list, sep='')
+print(res)
+
+
+# n = int(input('Введите целое число: '))
+# x = bin(n)
+# print(x)
+# print(int(x, 2))
