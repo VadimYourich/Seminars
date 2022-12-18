3388151bdd10e608c4216991003409effc721f2d
vadim <89281779909@mail.ru>
2022-12-18 21:19:05 +0300

    разницf между максимальным и минимальным значением дробной части элементов

--- seminar3/Home Work/task3.py
+++ seminar3/Home Work/task3.py
@@ -1,16 +1,20 @@
 # Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между
 # максимальным и минимальным значением дробной части элементов.
-#
 # Пример:
-#
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
+# list = [1.1, 1.2, 3.1, 5, 10.01]
+# list_dr = []
+# for i in range(0, len(list)):
+#     if (list[i] - int(list[i])) != 0:
+#         list_dr.append(round((list[i] - int(list[i])), 2))
+# max_dr = max(list_dr)
+# min_dr = min(list_dr)
+# print(list)
+# print((max_dr - min_dr))
+
 list = [1.1, 1.2, 3.1, 5, 10.01]
-list_dr = []
-for i in range(0, len(list)):
-    if (list[i] - int(list[i])) != 0:
-        list_dr.append(round((list[i] - int(list[i])), 2))
-max_dr = max(list_dr)
-min_dr = min(list_dr)
+list_dr = [round(abs(i - int(i)), 4) for i in list if int(i) != i]
 print(list)
-print((max_dr - min_dr))
+print(list_dr)
+print(max(list_dr) - min(list_dr))
