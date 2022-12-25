c39d3cc7ff9bf32071522f5963f505028c109537
vadim <89281779909@mail.ru>
2022-12-25 14:54:04 +0300

    можно ли из данных отрезков составить треугольник

--- seminar5/task2.py
+++ seminar5/task2.py
@@ -3,3 +3,12 @@
 # Входные данные	        Выходные данные
 # triangle(1, 1, 2)	    Это не треугольник
 # triangle(7, 6, 10)      Это треугольник
+
+a = 5
+b = 4
+c = 3
+def triangle(a, b, c):
+    if a + b > c and b + c > a and c + a >b:
+        return True
+    return False
+print(triangle(a, b, c))
