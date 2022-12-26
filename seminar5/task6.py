ede89a95cee4437d0614cd47d20ace4688bd37ca
vadim <89281779909@mail.ru>
2022-12-26 22:28:53 +0300

    не нравится лук)

--- seminar5/task6.py
+++ seminar5/task6.py
@@ -24,7 +24,7 @@ for i in range(1,6):
     sp.append(input())
 print(sp)
 
-sp = list(filter(lambda x:'лук' not  in x, sp))
+sp = list(filter(lambda x:'лук' not in x, sp))
 sp.sort(key = lambda x:len(x))
 print(*sp,sep = ', ')
 
