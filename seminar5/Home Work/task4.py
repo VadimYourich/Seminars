c1c5595cf9202067d0ae7f00dd9d054a8baf7201
vadim <89281779909@mail.ru>
2022-12-26 22:29:25 +0300

    hw

---
+++ seminar5/Home Work/task4.py
@@ -0,0 +1,6 @@
+# 4.	Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+# Пример:
+# Введите текст для кодировки: WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW
+# Текст после кодировки: 12W1B12W3B24W1B14W
+# Текст после дешифровки: WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW
+# Входные и выходные данные хранятся в отдельных текстовых файлах.
