b5db5311350d70ab59e12b924176f712802e4c1e
vadim <89281779909@mail.ru>
2022-12-26 19:44:28 +0300

    Дан список, вывести отдельно буквы и цифры.

--- seminar5/task5.py
+++ seminar5/task5.py
@@ -2,3 +2,9 @@
 # a = ( ‘1’, "a", 'b', '2', '3' ,'c')
 # b = ( 'a' , 'b' , 'c')
 # c = ( '1', '2', '3')
+
+sp = ['1', "a", 'b', '2', '3', 'c']
+a = list(filter(lambda x: x.isdigit(), sp))
+print(a)
+c = list(filter(lambda x: x.isalpha(), sp))
+print(c)
