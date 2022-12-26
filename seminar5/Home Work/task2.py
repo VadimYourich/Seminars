50c6f850c213e9728de36f8c1f70a8f237707370
vadim <89281779909@mail.ru>
2022-12-27 00:15:27 +0300

    конфетный бот

--- seminar5/Home Work/task2.py
+++ seminar5/Home Work/task2.py
@@ -6,22 +6,64 @@
 # a) Добавьте игру против бота
 # b) Подумайте как наделить бота "интеллектом"
 
+# import random
+# quantity = 121
+# max_step = 28
+# first = random.randint(1,2)
+# while quantity > max_step:
+#     while True:
+#         x = int(input(f'{first} игрок, заберите не более {max_step} конфет: '))
+#         if 0 < x <= max_step:
+#             quantity -= x
+#             print(f'Осталось {quantity} конфет')
+#             if first == 1:
+#                 first = 2
+#             elif first == 2:
+#                 first = 1
+#             break
+#         else:
+#             print(f'Максимальное количество конфет за один ход {max_step}!')
+# if first == 1:
+#     print('Победил 1-й игрок!')
+# else:
+#     print('Победил 2-й игрок!')
+
+
 import random
 quantity = 121
 max_step = 28
 first = random.randint(1,2)
 while quantity > max_step:
-    x = int(input(f'{first} игрок, заберите не более {max_step} конфет: '))
-    if 0 < x <= max_step:
-        quantity -= x
-        print(f'Осталось {quantity} конфет')
-        if first == 1:
-            first = 2
-        elif first == 2:
+    if first == 2:
+        if max_step + 1 < quantity <= max_step * 2 + 1:
+            x = quantity - 29
+            print(f'Бот забрал {x} конфет')
+            quantity -= x
+            print(f'Осталось {quantity} конфет')
+            first = 1
+        elif 0 < quantity <= max_step:
+            x = quantity
+            print(f'Бот забрал {x} конфет')
+            quantity -= x
+            print(f'Осталось {quantity} конфет')
+            first = 1
+        else:
+            x = random.randint(1,max_step)
+            print(f'Бот забрал {x} конфет')
+            quantity -= x
+            print(f'Осталось {quantity} конфет')
             first = 1
     else:
-        print(f'Максимальное количество конфет за один ход {max_step}!')
+        while True:
+            x = int(input(f'Заберите не более {max_step} конфет: '))
+            if 0 < x <= max_step:
+                quantity -= x
+                print(f'Осталось {quantity} конфет')
+                break
+            else:
+                print(f'Максимальное количество конфет за один ход {max_step}!')
+        first = 2
 if first == 1:
-    print('Победил 1-й игрок!')
+    print('Вы победили!')
 else:
-    print('Победил 2-й игрок!')
+    print('Победил Бот!')
