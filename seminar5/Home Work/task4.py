613bbc95ba5baa8a8deec6be0e4866c8f150208e
vadim <89281779909@mail.ru>
2022-12-29 13:01:40 +0300

    модуль сжатия и восстановления данных

--- seminar5/Home Work/task4.py
+++ seminar5/Home Work/task4.py
@@ -4,3 +4,37 @@
 # Текст после кодировки: 12W1B12W3B24W1B14W
 # Текст после дешифровки: WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+def encode(text):
+    res = ''
+    count = 1
+    for i in range(1, len(text)):
+        if text[i] == text[i - 1]:
+            count += 1
+        else:
+            res += str(count) + text[i - 1]
+            count = 1
+    res += str(count) + text[i]
+    return res
+
+def decode(text):
+    res = ''
+    digit = ''
+    for i in range(0, len(text)):
+        if text[i].isdigit():
+            digit += text[i]
+        else:
+            res += int(digit) * text[i]
+            digit = ''
+    return res
+
+if __name__ == '__main__':
+    with open('input.txt') as file:
+        text = file.readline().strip()
+    encode_text = encode(text)
+    print(encode_text)
+    with open('encode_output.txt', 'w') as file:
+        file.write(encode_text)
+    decode_text = decode(encode_text)
+    with open('decode_output.txt', 'w') as file:
+        file.write(decode_text)
