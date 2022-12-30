11b64175e573984d10de078dfcbef0114f22c02c
vadim <89281779909@mail.ru>
2022-12-30 13:49:53 +0300

    zip

--- seminar6/task4.py
+++ seminar6/task4.py
@@ -10,3 +10,14 @@
 # ['user1', 'user2', 'user3']
 # [4, 5, 9]
 # [111, 222, 333]
+
+users = ['user1', 'user2', 'user3', 'user4', 'user5']
+ids = [4, 5, 9, 14, 7]
+salary = [111, 222, 333]
+
+result = list(zip(users, ids, salary))
+print(result)
+
+res = list(zip(* result))
+print(res)
+
